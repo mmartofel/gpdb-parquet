d1cb838ebb708351aee26da08b1b71dfb924e7be
mmartofel <mmartofel@pivotal.io>
2020-04-28 08:38:44 +0200

    partitions rank

--- partitions_rank.sql
+++ partitions_rank.sql
@@ -1,3 +1,4 @@
 SELECT partitiontablename, partitionrank
 FROM   pg_partitions
-WHERE  partitiontablename like 'catalog_sales_hybrid%';
+WHERE  partitiontablename like 'catalog_sales_hybrid%'
+ORDER BY 2;
