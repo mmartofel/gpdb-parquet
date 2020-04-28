b5733a3701bc49d1eebd3442f31f62444c87239c
mmartofel <mmartofel@pivotal.io>
2020-04-28 10:34:33 +0200

    finish partition exchange

--- exchange_partitions.sql
+++ exchange_partitions.sql
@@ -1,2 +1,5 @@
-ALTER TABLE tpcds.catalog_sales_hybrid EXCHANGE PARTITION FOR (RANK(1)) WITH TABLE tpcds.catalog_sales_parquet_readable_ext_2 WITHOUT VALIDATION;
-ALTER TABLE catalog_sales_hybrid RENAME PARTITION FOR(RANK(1)) to "catalog_sales_hybrid_1_prt_2_ext_parquet";
+-- Run partition exchange followed by partition table rename:
+-- psql -v p1=2 -f exchange_partitions.sql
+
+ALTER TABLE tpcds.catalog_sales_hybrid EXCHANGE PARTITION FOR (RANK(1)) WITH TABLE tpcds.catalog_sales_parquet_readable_ext_:p1 WITHOUT VALIDATION;
+ALTER TABLE catalog_sales_hybrid RENAME PARTITION FOR(RANK(1)) to _ext_parquet;
