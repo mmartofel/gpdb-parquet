62951db54708b3cd0e0b7481fc6fc56ddf67853d
mmartofel <mmartofel@pivotal.io>
2020-04-28 09:14:23 +0200

    partitions rename

---
+++ exchange_partitions.sql
@@ -0,0 +1,2 @@
+ALTER TABLE tpcds.catalog_sales_hybrid EXCHANGE PARTITION FOR (RANK(1)) WITH TABLE tpcds.catalog_sales_parquet_readable_ext_2 WITHOUT VALIDATION;
+ALTER TABLE catalog_sales_hybrid RENAME PARTITION FOR(RANK(1)) to catalog_sales_hybrid_1_prt_2_ext_parquet;
