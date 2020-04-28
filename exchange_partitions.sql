33c1d3cf399eca2d8549189f16896798856e8586
mmartofel <mmartofel@pivotal.io>
2020-04-28 11:29:54 +0200

    update run.sh script 6

--- exchange_partitions.sql
+++ exchange_partitions.sql
@@ -1,10 +1,7 @@
 -- Run partition exchange followed by partition table rename:
 -- psql -v p1=2 -f exchange_partitions.sql
 
-DO $$
-DECLARE rank int;
-BEGIN
-rank = :p1 - 1;
+\set rank = :p1-1
+
 ALTER TABLE tpcds.catalog_sales_hybrid EXCHANGE PARTITION FOR (RANK(rank)) WITH TABLE tpcds.catalog_sales_parquet_readable_ext_:p1 WITHOUT VALIDATION;
 ALTER TABLE tpcds.catalog_sales_hybrid RENAME PARTITION FOR(RANK(rank)) to _ext_parquet;
-END $$;
