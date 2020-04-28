00a19cdf001072b1c78b3ee37cdc598aff43f7a7
mmartofel <mmartofel@pivotal.io>
2020-04-28 19:19:39 +0200

    update run.sh script final 2

--- partitions_rank.sql
+++ partitions_rank.sql
@@ -1,4 +1,4 @@
-SELECT schemaname, tablename, partitiontablename, partitionrank
+SELECT schemaname, tablename, partitiontablename, partitionrank, partitionboundary
 FROM   pg_partitions
 WHERE  tablename='catalog_sales_hybrid' and schemaname='tpcds'
 ORDER BY 4;
