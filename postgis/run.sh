bd83c669b123f327607549a7076f5577cd9c5d5a
mmartofel <mmartofel@pivotal.io>
2020-05-02 11:53:34 +0200

    add postgis

--- postgis/run.sh
+++ postgis/run.sh
@@ -1,8 +1,10 @@
-createdb nyc
+DBNAME=nyc
 
-psql -d myc -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/spatial_ref_sys.sql
-psql -d myc -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/postgis.sql
-psql -d myc -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/rtpostgis.sql
-psql -d myc -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/postgis_comments.sql
-psql -d myc -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/raster_comments.sql
-psql -d myc -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/topology_comments.sql
+createdb $DBNAME
+
+psql -d $DBNAME -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/spatial_ref_sys.sql
+psql -d $DBNAME -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/postgis.sql
+psql -d $DBNAME -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/rtpostgis.sql
+psql -d $DBNAME -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/postgis_comments.sql
+psql -d $DBNAME -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/raster_comments.sql
+psql -d $DBNAME -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/topology_comments.sql
