0a5e9fc757e4d5da98d748a9a27dc6c340d7648b
mmartofel <mmartofel@pivotal.io>
2020-05-02 11:49:26 +0200

    add postgis

---
+++ postgis/run.sh
@@ -0,0 +1,8 @@
+createdb nyc
+
+psql -d myc -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/spatial_ref_sys.sql
+psql -d myc -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/postgis.sql
+psql -d myc -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/rtpostgis.sql
+psql -d myc -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/postgis_comments.sql
+psql -d myc -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/raster_comments.sql
+psql -d myc -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/topology_comments.sql
