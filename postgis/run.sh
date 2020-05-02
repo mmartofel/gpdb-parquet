568b30309d99c3ab9a2cf4e38563f2d9dc06f429
mmartofel <mmartofel@pivotal.io>
2020-05-02 12:03:05 +0200

    add postgis fix

--- postgis/run.sh
+++ postgis/run.sh
@@ -1,10 +1,20 @@
 DBNAME=nyc
 
+-- Recreate database
+dropdb $DBNAME
 createdb $DBNAME
 
+-- Enable PostGIS at the database
 psql -d $DBNAME -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/postgis.sql
 psql -d $DBNAME -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/spatial_ref_sys.sql
 psql -d $DBNAME -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/rtpostgis.sql
 psql -d $DBNAME -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/postgis_comments.sql
 psql -d $DBNAME -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/raster_comments.sql
 psql -d $DBNAME -f /usr/local/greenplum-db/share/postgresql/contrib/postgis-2.1/install/topology_comments.sql
+
+-- Load data
+psql -d $DBNAME -f ./nyc_census_blocks.sql
+psql -d $DBNAME -f ./nyc_census_sociodata.sql
+psql -d $DBNAME -f ./nyc_neighborhoods.sql
+psql -d $DBNAME -f ./nyc_streets.sql
+psql -d $DBNAME -f ./nyc_subway_stations.sql
